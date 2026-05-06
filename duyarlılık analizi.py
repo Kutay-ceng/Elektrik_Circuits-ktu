718ad1d892cb0eeca555eb75ec99463d0691c678
Kutay Keleş <keleskutay61@gmail.com>
2026-05-06 22:25:58 +0300

    update

--- duyarlılık analizi.py
+++ duyarlılık analizi.py
@@ -5,19 +5,22 @@ def duyarlilik_katsayilarini_hesapla(R1, R2, R3, R4, Ig1, Ig2, v1, v2):
     
     Delta = (a * c) - (b**2)
     
-    S_R1_v1 = c / (Delta * R1)
+    S_R1_v1 = -(c / Delta) * (1 / (R1**2)) * v1
     S_R1_v2 = v1 / (v2 * Delta * R1 * R2)
     
-    S_R2_v1 = ((v1 - v2) * (c + b)) / (v1 * Delta * R2)
-    S_R2_v2 = ((v2 - v1) * (a + b)) / (v2 * Delta * R2)
+    S_R2_v1 = -((c + b) / Delta) * (1 / (R2**2)) * (v1 - v2)
+    S_R2_v2 = -((a + b) / Delta) * (1 / (R2**2)) * (v2 - v1)
     
-    S_Ig1_v1 = (Ig1 / v1) * (-c / Delta)
-    S_Ig2_v1 = (Ig2 / v1) * (-b / Delta)
+    S_Ig1_v1 = (-c / Delta)
+    S_Ig2_v1 = (-b / Delta)
 
+    S_Ig1_v2 = -b / Delta
+    S_Ig2_v2 = a / Delta
     return {
         "S_R1_v1": S_R1_v1, "S_R1_v2": S_R1_v2,
         "S_R2_v1": S_R2_v1, "S_R2_v2": S_R2_v2,
-        "S_Ig1_v1": S_Ig1_v1, "S_Ig2_v1": S_Ig2_v1
+        "S_Ig1_v1": S_Ig1_v1, "S_Ig2_v1": S_Ig2_v1,
+        "S_Ig1_v2": S_Ig1_v2, "S_Ig2_v2": S_Ig2_v2,
     }
 if __name__ == "__main__":
     R1, R2, R3, R4 = 25.0, 5.0, 50.0, 75.0
@@ -31,3 +34,4 @@ if __name__ == "__main__":
     
     for duyarlılık_katsayi, degeri in sonuclar.items():
         print(f"{duyarlılık_katsayi} = {degeri:.4f}")
+
