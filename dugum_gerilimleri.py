db8e8693b8193ebb6a38a40029def92d7d01da3e
Kutay Keleş <keleskutay61@gmail.com>
2026-05-06 15:18:41 +0300

    Add files via upload

---
+++ dugum_gerilimleri.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def devreyi_coz(r1, r2, r3, r4, ig1, ig2):
+    a = (1/r1) + (1/r2)
+    b = -(1/r2)
+    c = (1/r2) + (1/r3) + (1/r4)
+
+    # A * x = B formatında matris kurulumu
+    katsayi_matrisi = np.array([[a, b], 
+                                [b, c]])
+    
+    akimlar = np.array([-ig1, ig2])
+
+    gerilimler = np.linalg.solve(katsayi_matrisi, akimlar)
+    
+    return gerilimler[0], gerilimler[1]
+
+if __name__ == "__main__":
+    R1, R2, R3, R4 = 25.0, 5.0, 50.0, 75.0
+    Ig1, Ig2 = 12.0, 16.0
+
+    print("--- Gerilimler ---")
+    v1, v2 = devreyi_coz(R1, R2, R3, R4, Ig1, Ig2)
+    print(f"Hesaplanan V1: {v1:.4f} Volt")
+    print(f"Hesaplanan V2: {v2:.4f} Volt")
+
+    v1_yeni, v2_yeni = devreyi_coz(R1 * 1.1, R2, R3, R4, Ig1, Ig2)
+    print(f"\nR1 %10 artinca yeni V1: {v1_yeni:.4f} V")
+    print(f"V1'deki degisim: {v1_yeni - v1:.4f} V")
+    print(f"\nR1 %10 artinca yeni V2: {v2_yeni:.4f} V")
+    print(f"V2'deki degisim: {v2_yeni - v2:.4f} V")
---
+++ duyarlılık analizi.py
@@ -0,0 +1,33 @@
+def duyarlilik_katsayilarini_hesapla(R1, R2, R3, R4, Ig1, Ig2, v1, v2):
+    a = (1/R1) + (1/R2)
+    b = -(1/R2)
+    c = (1/R2) + (1/R3) + (1/R4)
+    
+    Delta = (a * c) - (b**2)
+    
+    S_R1_v1 = c / (Delta * R1)
+    S_R1_v2 = v1 / (v2 * Delta * R1 * R2)
+    
+    S_R2_v1 = ((v1 - v2) * (c + b)) / (v1 * Delta * R2)
+    S_R2_v2 = ((v2 - v1) * (a + b)) / (v2 * Delta * R2)
+    
+    S_Ig1_v1 = (Ig1 / v1) * (-c / Delta)
+    S_Ig2_v1 = (Ig2 / v1) * (-b / Delta)
+
+    return {
+        "S_R1_v1": S_R1_v1, "S_R1_v2": S_R1_v2,
+        "S_R2_v1": S_R2_v1, "S_R2_v2": S_R2_v2,
+        "S_Ig1_v1": S_Ig1_v1, "S_Ig2_v1": S_Ig2_v1
+    }
+if __name__ == "__main__":
+    R1, R2, R3, R4 = 25.0, 5.0, 50.0, 75.0
+    Ig1, Ig2 = 12.0, 16.0
+
+    v1_nom = 25
+    v2_nom = 90
+
+    print("---- Duyarlılık Katsayıları Aşağıdaki Gibidir ----")
+    sonuclar = duyarlilik_katsayilarini_hesapla(R1, R2, R3, R4, Ig1, Ig2, v1_nom, v2_nom)
+    
+    for duyarlılık_katsayi, degeri in sonuclar.items():
+        print(f"{duyarlılık_katsayi} = {degeri:.4f}")
