3626bed25f34b6366fd3b5c5505a4c6042b436c0
Kutay Keleş <keleskutay61@gmail.com>
2026-05-07 16:11:53 +0300

    An addition was made regarding the currents.

--- dugum_gerilimleri.py
+++ dugum_gerilimleri.py
@@ -28,3 +28,8 @@ if __name__ == "__main__":
     print(f"V1'deki degisim: {v1_yeni - v1:.4f} V")
     print(f"\nR1 %10 artinca yeni V2: {v2_yeni:.4f} V")
     print(f"V2'deki degisim: {v2_yeni - v2:.4f} V")
+
+    v1_yeni_I, v2_yeni_I = devre_cozumu(R1, R2, R3, R4, 13.0, Ig2)
+    print(f"Yeni V1: {v1_yeni_I:.4f} V (Degisim: {v1_yeni_I - v1:.4f} V)")
+    print(f"Yeni V2: {v2_yeni_I:.4f} V (Degisim: {v2_yeni_I - v2:.4f} V)")
+
